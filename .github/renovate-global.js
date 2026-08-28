a7215fc04f454dab7742a3f16762dc506c9eb7b4
screwys <screwygit@proton.me>
2026-08-28 19:54:30 +0300

    fix(deps): separate Renovate dependency updates

--- .github/renovate-global.js
+++ .github/renovate-global.js
@@ -3,9 +3,6 @@ module.exports = {
   repositories: ["screwys/Rufin"],
   onboarding: false,
   requireConfig: "required",
-  gitIgnoredAuthors: [
-    "renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>",
-  ],
   allowedCommands: [
     "^cargo run --locked -p xtask -- generate flatpak-sources$",
   ],
